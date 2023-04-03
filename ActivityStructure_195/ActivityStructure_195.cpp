8b9beab02e47bebaaa83e6e3adc5f2703b87eb94
WildanLikeTamago <utoyowildan13039@gmail.com>
2023-04-03 14:38:31 +0700

    Membuat input data Mahasiswa 2

--- ActivityStructure_195/ActivityStructure_195.cpp
+++ ActivityStructure_195/ActivityStructure_195.cpp
@@ -18,4 +18,15 @@ int main()
 	mhs1.alamat = "Subang";
 	mhs1.umur = 18;
 
+	cout << "Masukkan nim: ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama: ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat: ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan umur: ";
+	cin >> mhs2.umur;
+
+
+
 }
