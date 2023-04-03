75abb9a0426d1dc5c7b171a8318bc4eb9284392c
WildanLikeTamago <utoyowildan13039@gmail.com>
2023-04-03 14:33:20 +0700

    Membuat fungsi main Mahasiswa

--- ActivityStructure_195/ActivityStructure_195.cpp
+++ ActivityStructure_195/ActivityStructure_195.cpp
@@ -8,3 +8,14 @@ struct Mahasiswa {
 	string alamat;
 	int umur;
 };
+
+int main() 
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "20220140195";
+	mhs1.nama = "Wildan";
+	mhs1.alamat = "Subang";
+	mhs1.umur = 18;
+
+}
