431682f977914bb263b2dc408d9c0ec5a6f889b7
WildanLikeTamago <utoyowildan13039@gmail.com>
2023-04-03 14:53:27 +0700

    Membuat struktur alamat di dalam struktur mahasiswa

---
+++ ActivityStructure2_195/ActivityStructure2_195.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+struct AlamatDetail
+{
+	string desa;
+	string kota;
+
+};
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	AlamatDetail alamat;
+	int umur;
+};
